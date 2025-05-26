916df7f45f1fafb9859bbb39cf578a36c791a34e
Kencho5 <giokenchadze@gmail.com>
2025-05-26 14:07:34 +0400

    update: deployment script

--- push.sh
+++ push.sh
@@ -1,3 +1,3 @@
 docker build -t vecstore .
-docker tag vecstore-extractor:latest 355673447504.dkr.ecr.eu-central-1.amazonaws.com/vecstore-extractor:latest
-docker push 355673447504.dkr.ecr.eu-central-1.amazonaws.com/vecstore-extractor:latest
+docker tag vecstore-back:latest 355673447504.dkr.ecr.eu-central-1.amazonaws.com/vecstore-back:latest
+docker push 355673447504.dkr.ecr.eu-central-1.amazonaws.com/vecstore-back:latest
