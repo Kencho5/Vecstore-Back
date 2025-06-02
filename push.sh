bea363e3afafaeb8376469764b82ccd69d91c108
Kencho5 <giokenchadze@gmail.com>
2025-06-02 13:37:42 +0400

    add: deployment

--- push.sh
+++
@@ -1,4 +0,0 @@
-aws ecr get-login-password --profile kencho --region eu-central-1 | docker login --username AWS --password-stdin 355673447504.dkr.ecr.eu-central-1.amazonaws.com
-docker build -t vecstore-back .
-docker tag vecstore-back:latest 355673447504.dkr.ecr.eu-central-1.amazonaws.com/vecstore-back:latest
-docker push 355673447504.dkr.ecr.eu-central-1.amazonaws.com/vecstore-back:latest
