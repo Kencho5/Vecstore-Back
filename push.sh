8fc1163a0a5d12497948cac2fbaa41327b74da7e
Kencho5 <giokenchadze@gmail.com>
2025-06-03 16:28:16 +0400

    update: push script

--- push.sh
+++ push.sh
@@ -1,3 +1,4 @@
-cargo zigbuild --release --target aarch64-unknown-linux-gnu
-rsync -avz --rsync-path="sudo rsync" target/aarch64-unknown-linux-gnu/release/vecstore-back vecstore:/home/ubuntu/vecstore-back/
-ssh vecstore 'sudo chmod +x /home/ubuntu/vecstore-back/vecstore-back && sudo systemctl restart vecstore'
+aws ecr get-login-password --profile kencho --region eu-central-1 | docker login --username AWS --password-stdin 355673447504.dkr.ecr.eu-central-1.amazonaws.com
+docker build -t vecstore-back .
+docker tag vecstore-back:latest 355673447504.dkr.ecr.eu-central-1.amazonaws.com/vecstore-back:latest
+docker push 355673447504.dkr.ecr.eu-central-1.amazonaws.com/vecstore-back:latest
