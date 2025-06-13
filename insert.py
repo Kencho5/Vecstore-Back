18a7e64d630585db026840238c5152244a574e66
Kencho5 <giokenchadze@gmail.com>
2025-06-13 21:06:54 +0400

    add: insert test

---
+++ insert.py
@@ -0,0 +1,33 @@
+import requests
+import time
+from multiprocessing import Process
+
+def get_image_data(url):
+    try:
+        r = requests.get(url)
+        r.raise_for_status()
+        return r.content
+    except:
+        return None
+
+API_KEY = "58315b813632e7469c99b0809393e9ec8bfeb2fa7a8f71d534a0b56f656bbb2e"
+IMAGE_URL = "https://cdn.outsideonline.com/wp-content/uploads/2023/03/Funny_Dog_H.jpg?crop=25:14&width=500&enable=upscale"
+FILENAME = "image.jpg"
+DATABASE = "vecstore"
+IMG_DATA = get_image_data(IMAGE_URL)
+
+def insert_image_loop():
+    while True:
+        data = {'filename': FILENAME, 'database': DATABASE}
+        files = {'image': (FILENAME, IMG_DATA, 'image/jpeg')}
+        headers = {"Authorization": API_KEY}
+        res = requests.post("http://localhost:3000/insert", headers=headers, data=data, files=files)
+        print(f"Inserted | Status: {res.status_code}")
+
+if __name__ == "__main__":
+    num_processes = 6
+    processes = [Process(target=insert_image_loop) for _ in range(num_processes)]
+    for p in processes:
+        p.start()
+    for p in processes:
+        p.join()
