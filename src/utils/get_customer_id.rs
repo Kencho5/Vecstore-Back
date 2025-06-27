f0f2ac2c395abeb1b849a09858606d496c0163e2
Kencho5 <giokenchadze@gmail.com>
2025-06-27 13:02:21 +0400

    fix: portal url

--- src/utils/get_customer_id.rs
+++ src/utils/get_customer_id.rs
@@ -3,7 +3,7 @@ use sqlx::Error;
 
 pub async fn get_customer_id(email: String, pool: &PgPool) -> Result<String, Error> {
     let customer_id: String =
-        sqlx::query_scalar("SELECT customer_id FROM subscriptions WHERE user_email = $1")
+        sqlx::query_scalar("SELECT customer_id FROM transactions WHERE user_email = $1")
             .bind(email)
             .fetch_one(pool)
             .await?;
